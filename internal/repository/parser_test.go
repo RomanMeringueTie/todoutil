e885f795b609b4b179e501c1a0e3a7df36418aa3
RomanMeringueTie <romankochetkoveducation@gmail.com>
2026-04-02 19:05:31 +0700

    Change parser_tests

--- internal/repository/parser_test.go
+++ internal/repository/parser_test.go
@@ -25,31 +25,31 @@ func TestHasTodoSuffix(t *testing.T) {
 
 func TestParseLine(t *testing.T) {
 	var expected = model.NewTodo("Add Test for parseLine", model.InProgress, "parser.go")
-	var actual = parseLine("parser.go", "// TODO: Add Test for parseLine [*]", "TODO: ")
+	var actual = parseLine("parser.go", "// TASK: Add Test for parseLine [*]", "TASK: ")
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("expected: %v, actual: %v", expected, actual)
 	}
 
 	expected = model.NewTodo("Add Test for parseLine", model.Open, "parser.go")
-	actual = parseLine("parser.go", "// TODO: Add Test for parseLine [o]", "TODO: ")
+	actual = parseLine("parser.go", "// TASK: Add Test for parseLine [o]", "TASK: ")
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("expected: %v, actual: %v", expected, actual)
 	}
 
 	expected = model.NewTodo("Add Test for parseLine", model.Closed, "parser.go")
-	actual = parseLine("parser.go", "// TODO: Add Test for parseLine [x]", "TODO: ")
+	actual = parseLine("parser.go", "// TASK: Add Test for parseLine [x]", "TASK: ")
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("expected: %v, actual: %v", expected, actual)
 	}
 
 	expected = model.NewTodo("Add Test for parseLine", model.Open, "parser.go")
-	actual = parseLine("parser.go", "// TODO: Add Test for parseLine", "TODO: ")
+	actual = parseLine("parser.go", "// TASK: Add Test for parseLine", "TASK: ")
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("expected: %v, actual: %v", expected, actual)
 	}
 
 	expected = nil
-	actual = parseLine("parser.go", "func getTodoNameAndStatus(line string) (string, model.Status) {", "TODO: ")
+	actual = parseLine("parser.go", "func getTodoNameAndStatus(line string) (string, model.Status) {", "TASK: ")
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("expected: %v, actual: %v", expected, actual)
 	}
